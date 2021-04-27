590ccb2bd09e7b9a50a7a34339058662abf2c1e5
Abarna Pandian <abarnapandian2000@gmail.com>
2021-04-27 23:15:45 +0530

    Add files via upload

---
+++ Week8/Product of Two Numbers.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    double firstNumber, secondNumber, productOfTwoNumbers;
+    cin >> firstNumber >> secondNumber;
+    productOfTwoNumbers = firstNumber * secondNumber;  
+    cout << "Product = " << productOfTwoNumbers;
+    return 0;
+}
+
