9090808e4242d87874178c79a6b37c424ecd669a
Abarna Pandian <abarnapandian2000@gmail.com>
2021-05-20 12:45:26 +0530

    Update Triangle.cpp

--- Week1/Triangle.cpp
+++ Week1/Triangle.cpp
@@ -1,3 +1,35 @@
+/*Equilateral triangle: An equilateral triangle is a triangle in which all three sides are equal. In the familiar Euclidean geometry, equilateral triangles are also equiangular; that is, all three internal angles are also congruent to each other and are each 60°.
+
+Isosceles triangle: An isosceles triangle is a triangle that has two sides of equal length.
+
+Scalene triangle: A scalene triangle is a triangle that has three unequal sides, such as those illustrated above.
+
+Input Format
+
+50 50 60
+
+Constraints
+
+-
+
+Output Format
+
+This is an isosceles triangle.
+
+Sample Input 0
+
+50 50 60
+Sample Output 0
+
+This is an isosceles triangle.
+Sample Input 1
+
+90 80 70
+Sample Output 1
+
+This is a scalene triangle.
+*/
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
