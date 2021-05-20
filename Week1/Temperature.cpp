4c448231d44a4f371df1593568871a6d631443a6
Abarna Pandian <abarnapandian2000@gmail.com>
2021-05-20 12:41:07 +0530

    Update Temperature.cpp

--- Week1/Temperature.cpp
+++ Week1/Temperature.cpp
@@ -1,3 +1,32 @@
+/*Temp < 0 then Freezing weather Temp 0-10 then Very Cold weather Temp 10-20 then Cold weather Temp 20-30 then Normal in Temp Temp 30-40 then Its Hot Temp >=40 then Its Very Hot
+
+Input Format
+
+42
+
+Constraints
+
+-
+
+Output Format
+
+Its very hot.
+
+Sample Input 0
+
+42
+Sample Output 0
+
+Its very hot.
+Sample Input 1
+
+-32
+Sample Output 1
+
+Freezing weather.
+*/
+
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
