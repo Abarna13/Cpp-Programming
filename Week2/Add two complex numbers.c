b1a21865d47f00b044f504d5d93578687441f5d3
Abarna Pandian <abarnapandian2000@gmail.com>
2021-05-20 12:59:34 +0530

    Update Add two complex numbers.c

--- Week2/Add two complex numbers.c
+++ Week2/Add two complex numbers.c
@@ -1,3 +1,41 @@
+/*Create a C Program to Add Two Complex Numbers by Passing Structure to a Function
+
+Input Format
+
+2.1
+-2.3
+5.6
+23.2
+
+Constraints
+
+-
+
+Output Format
+
+Sum = 7.7 + 20.9i
+
+Sample Input 0
+
+2.1                                                                                                                       
+-2.3                                                                                                                      
+5.6                                                                                                                       
+23.2  
+Sample Output 0
+
+Sum = 7.7 + 20.9i
+Sample Input 1
+
+3.2                                                                                                                       
+-6.3                                                                                                                      
+5.5                                                                                                                       
+12.3 
+Sample Output 1
+
+Sum = 8.7 + 6.0i
+*/
+
+
 #include <stdio.h>
 typedef struct complex {
     float real;
