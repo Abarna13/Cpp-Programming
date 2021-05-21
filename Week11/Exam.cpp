ba3bb86359fa8e5801f78085e031794f7407e9cf
Abarna Pandian <abarnapandian2000@gmail.com>
2021-05-21 17:07:05 +0530

    Update Exam.cpp

--- Week11/Exam.cpp
+++ Week11/Exam.cpp
@@ -1,3 +1,42 @@
+/*
+Write a C++ program to declare whether student have passed or failed in the examination. An Exception is handled if the student has failed the examination.
+
+Input Format
+
+-
+
+Constraints
+
+--
+
+Output Format
+
+-
+
+Sample Input 0
+
+52                                                                                                                                           
+12                                                                                                                                           
+32                                                                                                                                           
+56                                                                                                                                           
+11                                                                                                                                           
+Sample Output 0
+
+You have failed the examination 
+Sample Input 1
+
+56                                                                                                                                           
+78                                                                                                                                           
+85                                                                                                                                           
+63                                                                                                                                           
+77                                                                                                                                           
+Sample Output 1
+
+You have passed with average of 71%
+*/
+ 
+
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
