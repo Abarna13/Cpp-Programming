860b5c1ff52608e61879e1885402410fb13dc2f1
Abarna Pandian <abarnapandian2000@gmail.com>
2021-05-21 15:51:14 +0530

    Update Area of rectangle.cpp

--- Week6/Area of rectangle.cpp
+++ Week6/Area of rectangle.cpp
@@ -1,3 +1,36 @@
+/*
+Create a class having two public data members and one member function which will return the area of the rectangle.
+
+Input Format
+
+60
+30
+
+Constraints
+
+-
+
+Output Format
+
+Area:1800
+
+Sample Input 0
+
+60                                                                                                                        
+30                                                                                                                        
+Sample Output 0
+
+Area:1800 
+Sample Input 1
+
+503                                                                                                                      
+230                                                                                                                      
+Sample Output 1
+
+Area:115690   
+*/
+
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
