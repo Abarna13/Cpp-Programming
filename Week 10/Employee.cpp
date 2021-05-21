7724477e959f38469139020a2cf783072f867557
Abarna Pandian <abarnapandian2000@gmail.com>
2021-05-21 17:01:09 +0530

    Update Employee.cpp

--- Week 10/Employee.cpp
+++ Week 10/Employee.cpp
@@ -1,3 +1,30 @@
+/*
+Write a C++ program for Employee management software for an organization. Consider RaiseSalary() and Promote() function as virtual function in Employee class(Base class). Specify the common salary raise of 1000 in RaiseSalary() and a promote code as 101 in Promote() in Employee class. Inherit a Manager class and apply the logic that Managers receive 30% increase from salary in RaiseSalary() and the promote code as 102 in Promote(). Display the salary for normal Employee, Managers and also the promote code of Manager.Access the memeber function by creating the reference pointer for Base class.
+
+Input Format
+
+-
+
+Constraints
+
+-
+
+Output Format
+
+-
+
+Sample Input 0
+
+5000
+Sample Output 0
+
+Common salary raise is 6000                                                                                                 
+Salary of managers 6500                                                                                                     
+Promote code is 102
+*/
+
+
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
