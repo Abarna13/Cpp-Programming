97e80194f4e3293b7cef230ee08556b310fa7887
Abarna Pandian <abarnapandian2000@gmail.com>
2021-05-21 15:17:32 +0530

    Update functions.c

--- Week4/functions.c
+++ Week4/functions.c
@@ -1,3 +1,32 @@
+/*Write a program that extracts and adds the two least significant digits of any number. Define three functions addTwoDigits(), lastDigit() and secondLastDigit(). The main function should call addTwoDigits(). The lastDigit() and secondLastDigit() functions should be called from function addTwoDigits().
+
+Input Format
+
+2134
+
+Constraints
+
+-
+
+Output Format
+
+Sum of last two digits is = 7
+
+Sample Input 0
+
+2134
+Sample Output 0
+
+Sum of last two digits is = 7   
+Sample Input 1
+
+5467
+Sample Output 1
+
+Sum of last two digits is = 13
+*/
+
+
 #include<stdio.h>
 int addtwodigit(int n);
 int lastdigit(int a);
