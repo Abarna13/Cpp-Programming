03998463e557f4902b1cb4bcf923b9e7f20af6d6
Abarna Pandian <abarnapandian2000@gmail.com>
2021-05-21 15:05:18 +0530

    Update Count frequency.c

--- Week3/Count frequency.c
+++ Week3/Count frequency.c
@@ -1,3 +1,42 @@
+/*Create a program in C to count the frequency of each element of an array.
+
+Input Format
+
+3 1 2 1
+
+Constraints
+
+-
+
+Output Format
+
+The frequency of all elements of array : 1 occurs 2 times 2 occurs 1 times
+
+Sample Input 0
+
+3
+1
+2
+1
+Sample Output 0
+
+The frequency of all elements of array : 
+1 occurs 2 times
+2 occurs 1 times
+Sample Input 1
+
+4
+1
+2
+1
+2
+Sample Output 1
+
+The frequency of all elements of array : 
+1 occurs 2 times
+2 occurs 2 times
+*/
+
 #include <stdio.h>
 int main()
 {
