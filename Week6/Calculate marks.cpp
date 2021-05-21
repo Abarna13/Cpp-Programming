fbfa9b10efa15baa9d8d0d7ef1906970c214d3c1
Abarna Pandian <abarnapandian2000@gmail.com>
2021-05-21 15:54:52 +0530

    Update Calculate marks.cpp

--- Week6/Calculate marks.cpp
+++ Week6/Calculate marks.cpp
@@ -1,3 +1,44 @@
+/*
+a c++ class called 'student' with
+
+Data members:
+
+name(char type),
+
+marks1,marks2 (integer type)
+
+The program asks the user to enter name and marks. Then calc_marks() calculates the media note and disp() display name and total average of mark on screen in different lines.
+
+Input Format
+
+saveetha 85 96
+
+Constraints
+
+-
+
+Output Format
+
+Student Details:
+name: saveetha Avg: 90
+
+Sample Input 0
+
+saveetha 85 96
+Sample Output 0
+
+Student Details:                                                                                                          
+name: saveetha   Avg: 90    
+Sample Input 1
+
+sam 75 85
+Sample Output 1
+
+Student Details:
+name: sam      Avg: 80
+*/
+
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
