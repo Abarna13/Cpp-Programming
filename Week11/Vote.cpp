b98bb864655aba5b5fcc619627231486978a6564
Abarna Pandian <abarnapandian2000@gmail.com>
2021-05-21 17:09:25 +0530

    Update Vote.cpp

--- Week11/Vote.cpp
+++ Week11/Vote.cpp
@@ -1,3 +1,26 @@
+/*
+Write a c++ program to grant the access for vote for the people with age above than 18 else deny it using user defined exception?
+
+Input Format
+
+15
+
+Constraints
+
+-
+
+Output Format
+
+Access denied for vote
+
+Sample Input 0
+
+15
+Sample Output 0
+
+Access denied for vote
+*/
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
