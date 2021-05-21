1c74358a134732c9d59e87f11ca2b4678e687f84
Abarna Pandian <abarnapandian2000@gmail.com>
2021-05-21 16:35:23 +0530

    Update Product of Two Numbers.cpp

--- Week8/Product of Two Numbers.cpp
+++ Week8/Product of Two Numbers.cpp
@@ -1,3 +1,33 @@
+/*
+Find the product of two numbers using single inheritance?
+
+Input Format
+
+3 4
+
+Constraints
+
+-
+
+Output Format
+
+Product = 12
+
+Sample Input 0
+
+5 2
+Sample Output 0
+
+Product = 10
+Sample Input 1
+
+3 4
+Sample Output 1
+
+Product = 12
+*/
+
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
