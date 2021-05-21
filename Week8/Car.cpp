541d75de724c5030f129ecd3142a154f1518a23d
Abarna Pandian <abarnapandian2000@gmail.com>
2021-05-21 16:37:36 +0530

    Update Car.cpp

--- Week8/Car.cpp
+++ Week8/Car.cpp
@@ -1,3 +1,29 @@
+/*
+Develop a C++ program based on the below class diagram.
+
+Input Format
+
+Reads no_of_litres Reads fuel_Amount Reads colour
+
+Constraints
+
+-
+
+Output Format
+
+-
+
+Sample Input 0
+
+10
+79
+Blue
+Sample Output 0
+
+Fuel amount is: 79
+Colour of the car is: Blue
+*/
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
