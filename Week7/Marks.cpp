e2300c3f18301e636723b6b7f51234974bb6654b
Abarna Pandian <abarnapandian2000@gmail.com>
2021-05-21 16:32:03 +0530

    Update Marks.cpp

--- Week7/Marks.cpp
+++ Week7/Marks.cpp
@@ -1,3 +1,32 @@
+/*
+Write a c++ program to get the marks from a person and display whether that particular person is passed or not passed using multilevel inheritance?
+
+Input Format
+
+60
+
+Constraints
+
+-
+
+Output Format
+
+You have not passed
+
+Sample Input 0
+
+60
+Sample Output 0
+
+You have not passed
+Sample Input 1
+
+75
+Sample Output 1
+
+You have passed
+*/
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
