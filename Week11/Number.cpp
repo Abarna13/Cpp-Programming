5d43f4975e05097a37b79146f8de3e6ed24b82c2
Abarna Pandian <abarnapandian2000@gmail.com>
2021-05-21 17:08:15 +0530

    Update Number.cpp

--- Week11/Number.cpp
+++ Week11/Number.cpp
@@ -1,3 +1,26 @@
+/*
+Find the sum of a number using user defined exception handling given Sum=10?
+
+Input Format
+
+80
+
+Constraints
+
+-
+
+Output Format
+
+Sum is:90
+
+Sample Input 0
+
+80
+Sample Output 0
+
+Sum is:90
+*/
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
