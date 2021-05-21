a8ed6cc62a8cd5898280dc73ab99c7d7b92b9904
Abarna Pandian <abarnapandian2000@gmail.com>
2021-05-21 15:08:17 +0530

    Update Count duplicate elements.c

--- Week3/Count duplicate elements.c
+++ Week3/Count duplicate elements.c
@@ -1,3 +1,40 @@
+/*Create a program in C to count a total number of duplicate elements in an array.
+
+Input Format
+
+4 3 4 4 3
+
+Constraints
+
+-
+
+Output Format
+
+The total number of duplicate elements found in the array is: 2
+
+Sample Input 0
+
+4
+3
+4
+4
+3
+Sample Output 0
+
+The total number of duplicate elements found in the array is: 2
+Sample Input 1
+
+3
+1
+2
+1
+Sample Output 1
+
+The total number of duplicate elements found in the array is: 1 
+*/
+
+
+
 #include <stdio.h>
 #define MAX_SIZE 100  
 int main()
